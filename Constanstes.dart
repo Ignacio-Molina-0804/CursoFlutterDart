7d2c8e54ef4ae686fdf615cb6a559eb0a863be24
Ignacio Molina <molinagranda@gmail.com>
2025-11-08 22:25:10 -0300

    Constantes

--- Constanstes.dart
+++ Constanstes.dart
@@ -1,5 +1,12 @@
+// Esto es una variable constante en Dart.
+
 void main() {
-  final PI = 3.1416;
+  final PI = 3.1416; // Puede depender de algo que pasa en tiempo de ejecución
+  const ValorFijo = 600; // Siempre es un valor fijo en tiempo de compilación
+
+  ValorFijo =
+      700; // Esto generará un error porque las constantes no pueden ser reasignadas
 
   print(PI);
+  print(ValorFijo);
 }
