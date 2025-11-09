19ab7617222c3bf2c52b18fcd12f0e510857d694
Ignacio Molina <molinagranda@gmail.com>
2025-11-08 22:23:59 -0300

    Concatenacion

---
+++ Concatenacion.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Declaración de múltiples variables en una sola línea
+  String nombre, apellido;
+  int edad, salario;
+
+  // Asignación de valores a las variables
+  nombre = "Ignacio";
+  apellido = "Molina";
+
+  edad = 20;
+  salario = 0;
+
+  // Concatenación e interpolación de cadenas
+  print("Nombre Completo: " + nombre + " " + apellido);
+  print("Edad: ${edad}");
+  print("Salario: ${salario}");
+}
