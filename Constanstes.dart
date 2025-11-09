488c017878c30da2a069658bbbb84e65a405998b
Ignacio Molina <molinagranda@gmail.com>
2025-11-08 22:00:49 -0300

    Constantes En Dart

---
+++ Constanstes.dart
@@ -0,0 +1,5 @@
+void main() {
+  final PI = 3.1416;
+
+  print(PI);
+}
--- VariablesYTiposDeDatos.dart
+++ VariablesYTiposDeDatos.dart
@@ -1,12 +1,17 @@
 void main() {
   String nombre;
   int edad;
-  double precio;
-  bool activo;
+  double precio = 550.20;
+  bool activo = false;
 
-  var codigo;
+  var codigo = "Informaticonfig";
 
   nombre = "Nacho";
+  edad = 20;
 
   print(nombre);
+  print(edad);
+  print(precio);
+  print(activo);
+  print(codigo);
 }
