3d932fc5b8db4d114a72c939434b38baa4f1a3bb
Ignacio Molina <molinagranda@gmail.com>
2025-11-09 21:13:49 -0300

    Operadores Relacionales Contenido

---
+++ Operadores/OperadoresRelacionales.dart
@@ -0,0 +1,21 @@
+void main() {
+  int valor1 = 90, valor2 = 55;
+
+  // Operador de Igualdad
+  print(valor1 == valor2);
+
+  // Operador de Desigualdad
+  print(valor1 != valor2);
+
+  // Operador Mayor que
+  print(valor1 > valor2);
+
+  // Operador Menor que
+  print(valor1 < valor2);
+
+  // Operador Mayor o Igual que
+  print(valor1 >= valor2);
+
+  // Operador Menor o Igual que
+  print(valor1 <= valor2);
+}
