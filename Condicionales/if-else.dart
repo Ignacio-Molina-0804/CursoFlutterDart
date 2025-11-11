f2c48ca5f4c6bde579d15874450479887b05837d
Ignacio Molina <molinagranda@gmail.com>
2025-11-10 22:39:38 -0300

    if Else

--- Condicionales/if-else.dart
+++ Condicionales/if-else.dart
@@ -1,5 +1,21 @@
+import 'dart:io';
+
 void main() {
+  print("Ingresa tu edad:");
+  int edad = int.parse(stdin.readLineSync()!);
 
-  
+  if (edad >= 18) {
+    print("Eres Mayor de Edad.");
+  } else if (edad >= 10) {
+    print("Eres un Adolescente.");
+  } else {
+    print("Eres Menor de Edad.");
+  }
 
+  int numero = 10;
+  String resultado = (numero % 2 == 0)
+      ? "El numero es Par."
+      : "El numero es impar.";
+
+  print(resultado);
 }
