96eb47e5b2bf2b7371bd1f446121c46e3578d1e9
Ignacio Molina <molinagranda@gmail.com>
2025-11-10 22:45:04 -0300

    Comentarios Operadores Logicos

--- Operadores/OperadoresLogicos.dart
+++ Operadores/OperadoresLogicos.dart
@@ -2,17 +2,20 @@ import 'dart:io';
 
 void main() {
 
+  // Ingreso de datos
   print("Ingresa tu edad:");
   int edad = int.parse(stdin.readLineSync()!);
   print("Tienes licencia?...");
   String tieneLicencia = stdin.readLineSync()!;
 
+  // Operador lógico AND (&&)
   if (edad >= 18 && tieneLicencia == "si") {
     print("Permiso de conducir otorgado.");
   } else {
     print("Permiso de conducir denegado.");
   }
 
+  // Operador lógico OR (||)
   if (edad < 18 || tieneLicencia != "si") {
     print("No cumples los requisitos para conducir.");
   } else {
