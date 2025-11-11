93fee769d24a00fcf704adf5347c3755709d1fcb
Ignacio Molina <molinagranda@gmail.com>
2025-11-10 22:40:22 -0300

    Comentarios If-else

--- Condicionales/if-else.dart
+++ Condicionales/if-else.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
 void main() {
+
+  // Ejemplo de if-else
   print("Ingresa tu edad:");
   int edad = int.parse(stdin.readLineSync()!);
 
+  // Uso de if, else y if-else
   if (edad >= 18) {
     print("Eres Mayor de Edad.");
   } else if (edad >= 10) {
@@ -12,6 +15,7 @@ void main() {
     print("Eres Menor de Edad.");
   }
 
+  // Ejemplo de operador ternario
   int numero = 10;
   String resultado = (numero % 2 == 0)
       ? "El numero es Par."
