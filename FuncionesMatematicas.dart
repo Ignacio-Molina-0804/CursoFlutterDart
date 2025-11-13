8c70007f83cae4491cdc23aa0a210ed63cc46f78
Ignacio Molina <molinagranda@gmail.com>
2025-11-12 21:28:40 -0300

    Funciones Matematicas

--- FuncionesMatematicas.dart
+++ FuncionesMatematicas.dart
@@ -2,24 +2,29 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
+
+    // Declaración de variables
     int a, b;
 
+    // Entrada de datos
     print("Ingrese el valor de A: ");
     a = int.parse(stdin.readLineSync()!);
     print("Ingrese el valor de B: ");
     b = int.parse(stdin.readLineSync()!);
 
-    print("Potencia de $a, elevado a la $b = ${pow(a, b)}");
-    print("Raíz cuadrada de $a = ${sqrt(a)}");
+    // Operaciones matemáticas
+    print("Potencia de $a, elevado a la $b = ${pow(a, b)}"); // a^b
+    print("Raíz cuadrada de $a = ${sqrt(a)}"); // √a
 
     double c = 8.23456789;
-    print(c.round());
-    print(c.floor());
+    print(c.round()); // Redondeo
+    print(c.floor()); // Redondeo hacia abajo
 
-    print(min(a, b));
-    print(max(a, b));
+    print(min(a, b)); // Mínimo
+    print(max(a, b)); // Máximo
 
-    var aleatorio = Random();
-    print(aleatorio.nextInt(100));
-    print(aleatorio.nextDouble());
+    // Números aleatorios
+    var aleatorio = Random(); // Instancia de generador de números aleatorios
+    print(aleatorio.nextInt(100)); // Número aleatorio entre 0 y 99
+    print(aleatorio.nextDouble()); // Número aleatorio entre 0.0 y 1.0
 }
