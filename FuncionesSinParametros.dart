7dda97c770d167182bd38cad0437894cfaca5396
Ignacio Molina <molinagranda@gmail.com>
2025-11-13 22:05:16 -0300

    Funciones sin parametros contenido

---
+++ FuncionesSinParametros.dart
@@ -0,0 +1,14 @@
+int valor1 = 50, valor2 = 100;
+
+int sumar() {
+  return valor1 + valor2;
+}
+
+String DimeNombre() {
+  return "IgnacioMolina";
+}
+
+void main() {
+  print(sumar());
+  print(DimeNombre());
+}
