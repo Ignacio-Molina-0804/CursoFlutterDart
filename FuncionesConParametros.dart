294380eb606a300d6ce6d75447b6d527949a39e6
Ignacio Molina <molinagranda@gmail.com>
2025-11-13 22:10:05 -0300

    FuncionConParametros Comentarios

--- FuncionesConParametros.dart
+++ FuncionesConParametros.dart
@@ -1,11 +1,14 @@
+// Creación de funciones con parámetros en Dart
 int Sumar(int a, int b) {
   return a + b;
 }
 
+// Función que recibe un parámetro de tipo cadena y no retorna valor
 void DimeNombre(String nombre) {
   print("Tu nombre es: $nombre");
 }
 
+// Función principal
 void main() {
   print(Sumar(90, 120));
   DimeNombre("Ignacio Molina");
