3166b99d2d09555d761b5eb8679474bdede0aaa7
Ignacio Molina <molinagranda@gmail.com>
2025-11-13 22:09:22 -0300

    FuncionesConParametros Contenido

---
+++ FuncionesConParametros.dart
@@ -0,0 +1,12 @@
+int Sumar(int a, int b) {
+  return a + b;
+}
+
+void DimeNombre(String nombre) {
+  print("Tu nombre es: $nombre");
+}
+
+void main() {
+  print(Sumar(90, 120));
+  DimeNombre("Ignacio Molina");
+}
