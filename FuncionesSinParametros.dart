575be2bc134cb149d05c30343bbfa0e8c2c4e653
Ignacio Molina <molinagranda@gmail.com>
2025-11-13 22:06:05 -0300

    Funciones Sin Parametros Comentarios

--- FuncionesSinParametros.dart
+++ FuncionesSinParametros.dart
@@ -1,13 +1,18 @@
+// Definición de variables globales
+
 int valor1 = 50, valor2 = 100;
 
+// Definición de función sin parámetros que retorna un entero
 int sumar() {
   return valor1 + valor2;
 }
 
+// Definición de función sin parámetros que retorna una cadena
 String DimeNombre() {
   return "IgnacioMolina";
 }
 
+// Función principal
 void main() {
   print(sumar());
   print(DimeNombre());
