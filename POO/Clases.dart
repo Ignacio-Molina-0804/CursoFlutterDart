ae6ef562d6bcb594cd738199aceb1f39edd8a767
Ignacio Molina <molinagranda@gmail.com>
2025-11-14 23:22:33 -0300

    Comentarios Clases

--- POO/Clases.dart
+++ POO/Clases.dart
@@ -1,19 +1,25 @@
+// Definición de la clase Persona
 class Persona {
+  // Atributos de la clase
   String? nombre, apellido;
   int? edad;
 
+  // Método para mostrar los datos de la persona
   void DimeDatos() {
     print("Nombre: $nombre $apellido");
     print("Edad: $edad");
   }
 }
 
+// Definición de la clase Animal
 class Animal {
+  // Atributos de la clase
   String? nombre;
   String? especie;
   double? tamanio;
   bool? desparasitado, vacunado;
 
+  // Método para mostrar los datos del animal
   void DimeDatos() {
     print('''Nombre: $nombre
     Especie: $especie
@@ -23,13 +29,17 @@ class Animal {
   }
 }
 
+// Función principal
 void main() {
+
+  // Creación de un objeto de la clase Persona
   Persona persona1 = Persona();
   persona1.nombre = "Ignacio";
   persona1.apellido = "Molina";
   persona1.edad = 25;
   persona1.DimeDatos();
 
+  // Creación de un objeto de la clase Animal
   Animal perro1 = Animal();
   perro1.nombre = "Firulais";
   perro1.especie = "Canino";
